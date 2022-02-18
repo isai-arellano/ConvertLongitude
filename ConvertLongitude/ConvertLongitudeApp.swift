7f9833a6c526962a2cff512491654560bdb4a879
Isai <isai.arellano@outlook.com>
2022-02-18 08:25:31 -0600

    Initial Commit

---
+++ ConvertLongitude/ConvertLongitudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertLongitudeApp.swift
+//  ConvertLongitude
+//
+//  Created by Isaí on 18/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ConvertLongitudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
